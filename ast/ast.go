6115a1d358446304774996424c06e8572ee3c141
lwlwilliam <lwlwilliam.a@gmail.com>
2019-07-22 23:26:39 +0800

    parse integer

--- ast/ast.go
+++ ast/ast.go
@@ -144,3 +144,21 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+// 数值
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {
+
+}
+
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
